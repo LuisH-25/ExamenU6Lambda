8cc95fd326bb26c50a537f204a725b65cd93dade
LuisH-25 <huanca.luis@pucp.pe>
2023-01-16 19:08:40 -0500

    comentando paymentData

--- lambda.py
+++ lambda.py
@@ -5,16 +5,17 @@ import mercadopago
  
 def lambda_handler(event, context): 
     sdk= mercadopago.SDK(os.environ["ACCESS_TOKEN"]) 
-    bodyGet = event 
+    #bodyGet = event 
     
-    payment_response = sdk.payment().create(bodyGet["paymentData"]) 
+    #payment_response = sdk.payment().create(bodyGet["paymentData"]) 
+    payment_response = sdk.payment().create(event) 
     payment = payment_response["response"] 
      
-    preference_response = sdk.preference().create(bodyGet["productData"]) 
-    preference = preference_response["response"] 
+    #preference_response = sdk.preference().create(bodyGet["productData"]) 
+    #preference = preference_response["response"] 
  
  
     return { 
         'statusCode': 200, 
-        'body': json.dumps(preference) 
+        'body': json.dumps(payment) 
     }
